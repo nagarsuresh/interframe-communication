ae39ee0a847b2b8adadfac4c4213cd708beaf232
Suresh Nagar <nagar.suresh@gmail.com>
2017-02-22 19:01:48 -0800

    event by type implementation

--- interframe_communication.js
+++ interframe_communication.js
@@ -1,19 +1,48 @@
 var IFC = (function () {
-    var listener;
-    function addListener(callback) {
-        listener = callback;
-    }
+    var listeners = [];
+    var isSelfIframe = window.self !== window.parent;
 
-    function publishEvent(msg) {
-        var frames = window.frames;
-        for (var i = 0; i < frames.length; i++) {
-            frames[i].postMessage(msg, "*");
+    function addListener(type, callback) {
+        if (listeners[type]) {
+            listeners[type].push(callback);
+        } else {
+            listeners[type] = [callback];
         }
     }
 
-    function receiveMessage(event) {
-        listener(event.data);
+    function publishEvent(msg) {
+        var framesList;
+        var publishToTop = false;
+        if (!isSelfIframe) {
+            framesList = window.frames;
+        } else {
+            framesList = window.top.frames;
+            publishToTop = true;
+        }
+
+        for (var i = 0; i < framesList.length; i++) {
+            if (framesList[i] !== window.self) {
+                framesList[i].postMessage(msg, "*");
+            }
+        }
+
+        if (publishToTop) {
+            window.top.postMessage(msg, "*");
+        }
+
+
     }
+
+    function receiveMessage(event) {
+        var data = event.data;
+        var callbacks = listeners[data.type];
+        if (callbacks) {
+            callbacks.forEach(c => {
+                c.apply(null, [data.payload]);
+            });
+        }
+    }
+
     window.addEventListener("message", receiveMessage, false);
 
 
